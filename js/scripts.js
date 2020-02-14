c85c2b0b9f307fa2221268936812130489d4c82d
Joan Nekoye <joannekoye@gmail.com>
2020-02-14 12:13:43 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function akanName (){
+  var welcome = document.getElementById("welcome").value;
+  var name = document.getElementById("name").value;
+  var birthDate = document.getElementById("date").value;
+  var gender= document.getElementById("gender").value;
+
+
+  welcome.innerHTML="welcome" +name.value
+}
