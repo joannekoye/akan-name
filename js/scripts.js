989dd7ca9bf1daddb1140ef1bb8df67d4869f932
Joan Nekoye <joannekoye@gmail.com>
2020-02-14 12:36:35 +0300

    add features to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,28 @@ function akanName (){
   var name = document.getElementById("name").value;
   var birthDate = document.getElementById("date").value;
   var gender= document.getElementById("gender").value;
+  var errorMessage = document.getElementById("errormessage");
+  var text;
+  errorMessage.style.padding = "10px";
+  if (name.length<1) {
+    text = "Please enter name"
+    errorMessage.innerHTML = text;
+    return false;
+  }
+  else {
+    welcome.innerHTML="welcome" +name.value
+  }
+  if (birthDate != NaN)
+  {
+    return true;
+  }
+  else {
+    text = "Please enter a valid date"
+    errorMessage.innerHTML = text
+    return false;
+  }
+  
 
-
-  welcome.innerHTML="welcome" +name.value
+  
 }
+  
