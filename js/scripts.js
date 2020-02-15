37d6b0c4e6c7289cc26b2d59287915b8ec74421a
Joan Nekoye <joannekoye@gmail.com>
2020-02-15 20:49:20 +0300

    Add arrays to js

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,9 @@ function akanName (){
   var yy= ((yyyy/100)-Math.floor(yyyy/100))*100
 
   var result = parseInt((((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7)
+  var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
+  var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+  var daysOfWeek = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
   
 
 
