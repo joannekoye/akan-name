ccd52131556d3222c29a304ffcadad08e2cefa57
Joan Nekoye <joannekoye@gmail.com>
2020-02-15 20:11:42 +0300

    Edit custom css style sheets

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,7 @@
 function akanName (){
-  var welcome = document.getElementById("welcome").value;
-  var name = document.getElementById("name").value;
-  var birthDate = document.getElementById("date").value;
-  var gender= document.getElementById("gender").value;
-  var errorMessage = document.getElementById("errormessage");
-  var text;
-  errorMessage.style.padding = "10px";
-  if (name.length<1) {
-    text = "Please enter name"
-    errorMessage.innerHTML = text;
-    return false;
-  }
-  else {
-    welcome.innerHTML="welcome" +name.value
-  }
-  if (birthDate != NaN)
-  {
-    return true;
-  }
-  else {
-    text = "Please enter a valid date"
-    errorMessage.innerHTML = text
-    return false;
-  }
-  
+  var mm= document.getElementById("birth-day").value
+  var dd= document.getElementById("birth-month").value
+  var yyyy= document.getElementById("birth-year").value
 
   
 }
