3b9a905e568cf632cb3f4d0e4dada835119e5a33
Joan Nekoye <joannekoye@gmail.com>
2020-02-15 20:40:04 +0300

    Work out logic in Js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 function akanName (){
-  var mm= document.getElementById("birth-day").value
-  var dd= document.getElementById("birth-month").value
-  var yyyy= document.getElementById("birth-year").value
+  var dd= parseInt(document.getElementById("birth-day").value)
+  var mm= parseInt(document.getElementById("birth-month").value)
+  var yyyy= parseInt(document.getElementById("birth-year").value)
 
+  var cc= parseInt((yyyy/100)+ 1)
+  var yy= ((yyyy/100)-Math.floor(yyyy/100))*100
+
+  var result = parseInt((((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7)
   
+
+
+
 }
   
