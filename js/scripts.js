a01390db7d9a756ac909adc8f877262eca2b4fb6
Joan Nekoye <joannekoye@gmail.com>
2020-02-15 22:02:48 +0300

    change century calculation

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ function akanName (){
   var mm= parseInt(document.getElementById("birth-month").value)
   var yyyy= parseInt(document.getElementById("birth-year").value)
 
-  var cc= parseInt((yyyy/100)+ 1)
-  var yy= ((yyyy/100)-Math.floor(yyyy/100))*100
+  var cc= parseInt(yyyy/100)
+  var yy= ((yyyy/100)-(Math.floor(yyyy/100)))*100
 
-  var result = parseInt((((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7)
+  var result = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7
   var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
   var femaleNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
   var daysOfWeek = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
@@ -64,6 +64,9 @@ function akanName (){
   }
   else if(result==6 && gender==='Female'){
     document.getElementById("result").innerHTML = "You were born on a " +daysOfWeek[6]+". Your Akan name is " + femaleNames[6];
+  }
+  else{
+    alert("error!")
   }
     
 
